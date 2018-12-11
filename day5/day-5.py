71a8fdc2e10a0053f35cf45fe8e45f593ba9101a
TheShrug <stewart.m.gordon@gmail.com>
2018-12-10 16:38:57 -0800

    Start of problem

---
+++ day5/day-5.py
@@ -0,0 +1,21 @@
+string = open("input.txt").read()
+
+
+current_index = 0
+
+while current_index < string.__len__():
+    print(current_index)
+
+    if current_index != string.__len__() - 1:
+        if string[current_index].islower():
+            if string[current_index + 1].isupper() and string[current_index + 1] == string[current_index].upper():
+                string = string[:current_index] + string[current_index + 1:]
+                current_index = 0
+        if string[current_index].isupper():
+            if string[current_index + 1].islower() and string[current_index + 1] == string[current_index].lower():
+                string = string[:current_index] + string[current_index + 1:]
+                current_index = 0
+
+    current_index += 1
+
+
