d946cdc83fc50243c2b692581ac5751a7835a701
TheShrug <stewart.m.gordon@gmail.com>
2019-02-05 11:18:48 -0800

    Day 9-2

--- day9/day9.py
+++ day9/day9.py
@@ -1,24 +1,28 @@
 num_players = 459
 num_marbles = 71790
 
-player_list = {i: 0 for i in range(num_players)}
-
-marble_circle_list = []
-
-current_index = 0
 
 # lots of off-by-one issues in this problem
-for i in range(num_marbles + 1):
-    if i % 23 == 0 and i != 0:
-        prev_index = (current_index - 7) % (marble_circle_list.__len__())
-        points = i + marble_circle_list.pop(prev_index)
-        player_list[i % num_players] += points
-        current_index = prev_index % marble_circle_list.__len__()
-    else:
-        if marble_circle_list.__len__() == 0:
-            marble_circle_list.insert(0, i)
+def play(number_of_marbles, number_of_players):
+    player_scores = {i: 0 for i in range(number_of_players)}
+    marble_circle_list = []
+    current_index = 0
+    for i in range(number_of_marbles + 1):
+        if i % 1000000 == 0:
+            print(i, 'million')
+        if i % 23 == 0 and i != 0:
+            prev_index = (current_index - 7) % (marble_circle_list.__len__())
+            points = i + marble_circle_list.pop(prev_index)
+            player_scores[i % num_players] += points
+            current_index = prev_index % marble_circle_list.__len__()
         else:
-            current_index = ((current_index + 1) % marble_circle_list.__len__()) + 1
-            marble_circle_list.insert(current_index, i)
+            if marble_circle_list.__len__() == 0:
+                marble_circle_list.insert(0, i)
+            else:
+                current_index = ((current_index + 1) % marble_circle_list.__len__()) + 1
+                marble_circle_list.insert(current_index, i)
+    return player_scores
 
-print(max(player_list.values()))
+
+print(max(play(num_marbles, num_players).values()))
+print(max(play(num_marbles * 100, num_players).values()))
